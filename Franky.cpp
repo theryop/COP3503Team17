7897962603917a02798b11d291dc42569bf6fe19
scottliu77 <scottliu77@gmail.com>
2016-04-19 22:06:39 -0400

    Restock works (no input validation), Sell not yet
    Sell still needs work. Check inv still works.

--- Franky.cpp
+++ Franky.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-class items {
+class items { //constructor for the item class
 private:
 	string itemName;
 	int itemPrice;
@@ -44,7 +44,7 @@ public:
 	}
 };
 
-class employees {
+class employees { //constructor for employee class
 private:
 	string empName;
 	string empUsername;
@@ -132,35 +132,38 @@ break;
 
 
 int main() {
-	vector<items> vectorOfItems;
-	vector<employees> vectorOfEmployees;
+	vector<items> vectorOfItems; //creates the vector of items
+	vector<employees> vectorOfEmployees; //creates the vector of employees
 
 	int currentEmployee;
 
+	//initial prompt for user to select employee or customer
 	cout << "Welcome to the shop! Are you an employee or customer?" << endl;
 	cout << "1. Employee" << endl;
 	cout << "2. Customer" << endl;
 	string userInput = "";
 	int firstInput = 0;
-	int hello = 0;
-	while (firstInput != 1 && firstInput != 2) {
+	bool hello = true;
+
+	while (firstInput != 1 && firstInput != 2) { //input validation for the first menu choice
 		bool isNum = true;
 		cout << endl;
 		cin >> userInput;
+		//the following for loop checks to see if the user inputed a number
 		for (size_t i = 0; i < userInput.length(); i++) {
 			if (!isdigit(userInput.at(i))) {
 				isNum = false;
 			}
 		}
+		//if the input is a number, convert the string to an int which will dictate where the code continues
 		if (isNum) {
 			firstInput = atoi(userInput.c_str());
 		}
-		if (firstInput == 1) {
-			if (hello == 0) {
+		if (firstInput == 1) { //entry points for the employee login
+			if (hello) { //only prints the "Hello Employee!" statement once so as to not sound redundant
 				cout << "Hello Employee! Please login or register for an account." << endl;
-				hello++;
+				hello = false;
 			}
-			
 			cout << "1. Login" << endl;
 			cout << "2. Register" << endl;
 			cout << "3. Exit" << endl;
@@ -201,141 +204,167 @@ int main() {
 
 
 						//might put code directly in here because it would be easier to append to vector, check username for sales... etc
-						while (true) {
-							cout << "What would you like to do?" << endl;
-							cout << "1. Restock" << endl;
-							cout << "2. Make a sale" << endl;
-							cout << "3. Check inventory" << endl;
-							cout << "4. Exit" << endl;
-							int loggedInInput;
-							cin >> loggedInInput;
+						bool loginFirstTime = true;
+						string userLoggedInInput = "";
+						int loggedInInput = 0;
+						bool isNum3 = true;
+							while (loggedInInput != 1 && loggedInInput != 2 && loggedInInput != 3 && loggedInInput != 4) {
+								if (loginFirstTime) {
+									cout << endl << "Login successful." << endl;
+								}
+								loginFirstTime = false;
+								cout << "What would you like to do?" << endl;
+								cout << "1. Restock" << endl;
+								cout << "2. Make a sale" << endl;
+								cout << "3. Check inventory" << endl;
+								cout << "4. Logout" << endl;
+								cout << endl;
+								cin >> userLoggedInInput;
+								for (size_t i = 0; i < userLoggedInInput.length(); i++) {
+									if (!isdigit(userLoggedInInput.at(i))) {
+										isNum3 = false;
+									}
+								}
+								if (isNum3) {
+									loggedInInput = atoi(userLoggedInInput.c_str());
+								}
+								if (loggedInInput == 1) { //PETER's CODE
+									//takes in input for creativity/activity/complexity
+									//creates item
+									//pushes on to vectorOfItems
 
-							if (loggedInInput == 1) { //PETER's CODE
-								//takes in input for creativity/activity/complexity
-								//creates item
-								//pushes on to vectorOfItems
-
-								//VALUES ARE TEMPORARY
-								int creativity = 0;
-								int activity = 0;
-								int complexity = 0;
-								string name = "";
-								string n = "";
-								// make name all lowercase
-								int price = 0;
-								cout << "What is the name of item you'd like to restock?" << endl;
-								cin >> n;
-								int counter = 0;
-								char c;
-								while (counter < n.length()) //converts all characters in string to lowercase
-								{
-									c = n[counter];
-									if (isupper(c))
+									//VALUES ARE TEMPORARY
+									int creativity = 0;
+									int activity = 0;
+									int complexity = 0;
+									string name = "";
+									string n = "";
+									// make name all lowercase
+									int price = 0;
+									cout << "What is the name of item you'd like to restock?" << endl;
+									cin >> n;
+									size_t counter = 0;
+									char c;
+									while (counter < n.length()) //converts all characters in string to lowercase
 									{
-										n[counter] = tolower(c);
+										c = n[counter];
+										if (isupper(c))
+										{
+											n[counter] = tolower(c);
 
+										}
+										counter++;
 									}
-									counter++;
-								}
-								name = n;
-								counter = 0;
-								int counter2 = 0;
-								int restockchoice = 0;
-								bool alreadychecked = false;
-								bool usingprevious = false;
-								vector<items> vectorOfCheckedItems;
-								while (counter < vectorOfItems.size()) // checks to see if an item with the same name has already been added to the inventory, and prompts user if they would like to use these stats
-								{
-									while (counter2 < vectorOfCheckedItems.size()) // checks to see if the item is the same as what has previously been offered
+									name = n;
+									counter = 0;
+									size_t counter2 = 0;
+									int restockchoice = 0;
+									bool alreadychecked = false;
+									bool usingprevious = false;
+									vector<items> vectorOfCheckedItems;
+									while (counter < vectorOfItems.size()) // checks to see if an item with the same name has already been added to the inventory, and prompts user if they would like to use these stats
 									{
-										if ((vectorOfCheckedItems[counter2].getItemName() == vectorOfItems[counter].getItemName()) && (vectorOfCheckedItems[counter2].getItemPrice() == vectorOfItems[counter].getItemPrice()) && (vectorOfCheckedItems[counter2].getItemCreativity() == vectorOfItems[counter].getItemCreativity()) && (vectorOfCheckedItems[counter2].getItemActivity() == vectorOfItems[counter].getItemActivity()) && (vectorOfCheckedItems[counter2].getItemComplexity() == vectorOfItems[counter].getItemComplexity()))
+										while (counter2 < vectorOfCheckedItems.size()) // checks to see if the item is the same as what has previously been offered
 										{
-											alreadychecked = true;
+											if ((vectorOfCheckedItems[counter2].getItemName() == vectorOfItems[counter].getItemName()) && (vectorOfCheckedItems[counter2].getItemPrice() == vectorOfItems[counter].getItemPrice()) && (vectorOfCheckedItems[counter2].getItemCreativity() == vectorOfItems[counter].getItemCreativity()) && (vectorOfCheckedItems[counter2].getItemActivity() == vectorOfItems[counter].getItemActivity()) && (vectorOfCheckedItems[counter2].getItemComplexity() == vectorOfItems[counter].getItemComplexity()))
+											{
+												alreadychecked = true;
+											}
+											counter2++;
 										}
-										counter2++;
+										if ((vectorOfItems[counter].getItemName() == name) && !alreadychecked)
+										{
+											cout << name << " has been found in our inventory with a price of " << vectorOfItems[counter].getItemPrice() << ", " << vectorOfItems[counter].getItemCreativity() << " Creativity, " << vectorOfItems[counter].getItemActivity() << " Activity, and " << vectorOfItems[counter].getItemComplexity() << " complexity. Would you like to use these statistics?" << endl;
+											cout << "1. Yes" << endl;
+											cout << "2. No" << endl;
+											cin >> restockchoice;
+											if (restockchoice == 1)
+											{
+												price = vectorOfItems[counter].getItemPrice();
+												creativity = vectorOfItems[counter].getItemCreativity();
+												activity = vectorOfItems[counter].getItemActivity();
+												complexity = vectorOfItems[counter].getItemComplexity();
+												usingprevious = true;
+												break;
+											}
+											else if (restockchoice == 2)
+											{
+												vectorOfCheckedItems.push_back(vectorOfItems[counter]);
+											}
+											else
+											{
+												//needs to add a validator for false choice
+											}
+										}
+
+										counter++;
 									}
-									if ((vectorOfItems[counter].getItemName() == name) && !alreadychecked)
+									if (!usingprevious)
 									{
-										cout << name << " has been found in our inventory with a price of " << vectorOfItems[counter].getItemPrice() << ", " << vectorOfItems[counter].getItemCreativity() << " Creativity, " << vectorOfItems[counter].getItemActivity() << " Activity, and " << vectorOfItems[counter].getItemComplexity() << " complexity. Would you like to use these statistics?" << endl;
-										cout << "1. Yes" << endl;
-										cout << "2. No" << endl;
-										cin >> restockchoice;
-										if (restockchoice == 1)
-										{
-											price = vectorOfItems[counter].getItemPrice();
-											creativity = vectorOfItems[counter].getItemCreativity();
-											activity = vectorOfItems[counter].getItemActivity();
-											complexity = vectorOfItems[counter].getItemComplexity();
-											usingprevious = true;
-											break;
-										}
-										else if (restockchoice == 2)
-										{
-											vectorOfCheckedItems.push_back(vectorOfItems[counter]);
-										}
-										else
-										{
-											//needs to add a validator for false choice
+										cout << "What is the selling price for this item?" << endl;
+										cin >> price;
+										cout << "What is the creativity value associated with the item? (Scale of 1-5)" << endl;
+										cin >> creativity;
+										cout << "What is the activity value associated with the item? (Scale of 1-5)" << endl;
+										cin >> activity;
+										cout << "What is the complexity value associated with the item? (Scale of 1-5)" << endl;
+										cin >> complexity;
+									}
+
+									vectorOfItems.push_back(items(name, price, creativity, activity, complexity));
+									cout << "Restocked!" << endl;
+									loggedInInput = 0; //need this so the loop continues until it exits
+								}
+
+								else if (loggedInInput == 2) {
+									//takes in input of item name
+									//removes first found item from vectorOfItems
+									//add to sale count of employee -> increments value of count for employee
+									// again this would be better in main function because we can keep track of employee with a variable equal to the username
+									string purchaseName;
+									cout << "What would you like to sell?" << endl;
+									cin >> purchaseName;
+									bool success = false;
+									// make all lowercase
+
+									for (size_t i = 0; i < vectorOfItems.size(); i++) {
+										if (purchaseName == vectorOfItems[i].getItemName()) {
+											vectorOfEmployees[currentEmployee].setEmpTotalSales((vectorOfEmployees[currentEmployee].getEmpTotalSales()) + 1); //need to  modify this to change it to be price + totalSales eventually
+											//deletes item in vector
+											vectorOfItems.erase(vectorOfItems.begin() + i);
+											cout << "Successful Transaction" << endl;
+											bool success = true;
 										}
 									}
 
-									counter++;
-								}
-								if (!usingprevious)
-								{
-									cout << "What is the selling price for this item?" << endl;
-									cin >> price;
-									cout << "What is the creativity value associated with the item? (Scale of 1-5)" << endl;
-									cin >> creativity;
-									cout << "What is the activity value associated with the item? (Scale of 1-5)" << endl;
-									cin >> activity;
-									cout << "What is the complexity value associated with the item? (Scale of 1-5)" << endl;
-									cin >> complexity;
-								}
-
-								vectorOfItems.push_back(items(name, price, creativity, activity, complexity));
-								cout << "Restocked!" << endl;
-							}
-
-							else if (loggedInInput == 2) {
-								//takes in input of item name
-								//removes first found item from vectorOfItems
-								//add to sale count of employee -> increments value of count for employee
-								// again this would be better in main function because we can keep track of employee with a variable equal to the username
-								string purchaseName;
-								bool success = false;
-								// make all lowercase
-
-								for (size_t i = 0; i < vectorOfItems.size(); i++) {
-									if (purchaseName == vectorOfItems[i].getItemName()) {
-										vectorOfEmployees[currentEmployee].setEmpTotalSales((vectorOfEmployees[currentEmployee].getEmpTotalSales()) + 1); //need to  modify this to change it to be price + totalSales eventually
-										//deletes item in vector
-										vectorOfItems.erase(vectorOfItems.begin() + i);
-										cout << "Successful Transaction" << endl;
-										bool success = true;
+									if (!success) {
+										cout << "Failed to sell item!" << endl;
 									}
+									loggedInInput = 0;
 								}
 
-								if (!success) {
-									cout << "Failed to sell item!" << endl;
+								else if (loggedInInput == 3) {
+									//loops through vectorOfItems and prints each itemName and itemPrice
+									cout << "Current in stock: " << endl;
+									for (size_t i = 0; i < vectorOfItems.size(); i++) {
+										cout << "Item: " << vectorOfItems[i].getItemName() << " Price: " << vectorOfItems[i].getItemPrice() << endl;
+									}
+									loggedInInput = 0;
 								}
-							}
 
-							else if (loggedInInput == 3) {
-								//loops through vectorOfItems and prints each itemName and itemPrice
-								cout << "Current in stock: " << endl;
-								for (size_t i = 0; i < vectorOfItems.size(); i++) {
-									cout << "Item: " << vectorOfItems[i].getItemName() << " Price: " << vectorOfItems[i].getItemPrice() << endl;
+								else if (loggedInInput == 4) {
+									cout << "Logout successful." << endl;
+									secondInput = 0;							
 								}
-							}
-
-							else if (loggedInInput == 4) {
-								break;
-							}
-						} // end of while loop for user logged in
+								else {
+									cout << "Error. Not a valid choice. Please try again." << endl;
+								}
+							} //end of Employee options loop
+						//} // end of while loop for user logged in GIVING PROBLEMS RN
 					}
 					else { //if (!isAuthenticated)
 						secondInput = 0;
+						hello = true;
 						cout << endl << "Login Unsuccesful. Invalid username or password. Please try again." << endl;
 					}
 				}
@@ -370,18 +399,22 @@ int main() {
 					}
 					else {
 						//prints and loop will run again
-						cout << "Username already taken!" << endl;
+						cout << "Username already taken!" << endl << endl;
 						secondInput = 0;
 					}
 				}
 
 				else if (secondInput == 3) {
-					break; //doesnt work as intended rn
+					firstInput = 0;
+					cout << "Welcome to the shop! Are you an employee or customer?" << endl;
+					cout << "1. Employee" << endl;
+					cout << "2. Customer" << endl;
+					hello = true;
+					break;
 				}
 				else {
 					cerr << "Error, not a valid choice. Please enter either 1 to login, 2 to register, or 3 to go back." << endl;
 				}
-				cout << "Hello Employee! Please login or register for an account." << endl;
 				cout << "1. Login" << endl;
 				cout << "2. Register" << endl;
 				cout << "3. Exit" << endl;
